4bd51815a60d292b3f70094c58c90aa1d03157fb
Francois Polo <andasan@gmail.com>
2021-03-15 11:57:34 -0700

    redux reselect

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React, { Component } from 'react';
 
 import Persons from './containers/Persons';
 import Count from './containers/Count'
+import Posts from './containers/Posts';
 
 class App extends Component {
   render() {
     return (
       <div className="App">
         <Count />
+        <Posts />
         <Persons />
       </div>
     );
--- src/containers/Posts.js
+++ src/containers/Posts.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react'
 import { connect } from 'react-redux'
+import {selectPostsArray} from '../redux/post.selector'
 
 let count = 0;
 
@@ -8,11 +9,24 @@ class Post extends Component{
     render(){
         console.log(`Posts render ${++count}`)
         return(
-            <>
-            </>
+            <div>
+              <h3>Posts</h3>  
+              <ul>
+                  {
+                      this.props.postsArr.map(post => (
+                          <li key={post.id}>{post.title}</li>
+                      ))
+                  }
+              </ul>
+            </div>
         )
     }
 
 }
 
-export default connect()(Post)
+const mapStateToProps = (state) => ({ 
+    // postsArr: state.postsObj.posts
+    // postsArr: state.postsObj.posts.filter(post => post.author === 'user-1')
+    postsArr: selectPostsArray(state)
+})
+export default connect(mapStateToProps)(Post)
